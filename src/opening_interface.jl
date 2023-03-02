d7904716213b7620d585fe30bede19c5435a5d98
mattar13 <mtarchick13@gmail.com>
2023-03-02 17:38:16 -0500

    renamed source

--- src/opening_interface.jl
+++ src/opening_interface.jl
@@ -1,6 +1,6 @@
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
-run_experiment_analysis() = Pluto.run(notebook="src/Interface/experiment_analysis.jl")
-run_datasheet_analysis() = Pluto.run(notebook="src/Interface/datasheet_analysis.jl")
-run_trace_analysis() = Pluto.run(notebook="src/Interface/file_analysis.jl") #A-wave analysis for a file of traces
-run_filter_determination() = Pluto.run(notebook = "src/Interface/filter_determination.jl") #A-wave analysis for a file of traces
-run_subtraction_analysis() = Pluto.run(notebook = "src/Interface/subtraction_analysis.jl")
+run_experiment_analysis() = Pluto.run(notebook="src\\experiment_analysis.jl")
+run_datasheet_analysis() = Pluto.run(notebook="src\\datasheet_analysis.jl")
+run_trace_analysis() = Pluto.run(notebook="src\\file_analysis.jl") #A-wave analysis for a file of traces
+run_filter_determination() = Pluto.run(notebook = "src\\filter_determination.jl") #A-wave analysis for a file of traces
+run_subtraction_analysis() = Pluto.run(notebook = "src\\subtraction_analysis.jl")
