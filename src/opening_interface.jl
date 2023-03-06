fbb1ac0cc4132c8b045cae45902b16938b9213a5
mattar13 <mtarchick13@gmail.com>
2023-03-06 11:52:34 -0500

    opening_interface is changed

--- src/opening_interface.jl
+++ src/opening_interface.jl
@@ -1,6 +1,6 @@
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
 run_experiment_analysis() = Pluto.run(notebook="src\\experiment_analysis.jl")
 run_datasheet_analysis() = Pluto.run(notebook="src\\datasheet_analysis.jl")
-run_trace_analysis() = Pluto.run(notebook="src\\file_analysis.jl") #A-wave analysis for a file of traces
+run_trace_analysis() = Pluto.run(notebook="src\\trace_analysis.jl") #A-wave analysis for a file of traces
 run_filter_determination() = Pluto.run(notebook = "src\\filter_determination.jl") #A-wave analysis for a file of traces
 run_subtraction_analysis() = Pluto.run(notebook = "src\\subtraction_analysis.jl")
