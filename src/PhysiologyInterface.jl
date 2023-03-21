f5587a9b5cf75afeef56d624db5577f089db8a41
mattar13 <mtarchick13@gmail.com>
2023-03-21 09:26:43 -0400

    moved notebooks to a seperate folder

--- src/PhysiologyInterface.jl
+++ src/PhysiologyInterface.jl
@@ -1,17 +1,4 @@
 module PhysiologyInterface
 
-# Write your package code here.
-using Pkg
-
-using ElectroPhysiology
-using PhysiologyAnalysis
-using Pluto
-
-include("opening_interface.jl")
-export run_experiment_analysis
-export run_datasheet_analysis
-export run_trace_analysis
-export run_filter_determination
-export run_subtraction_analysis
 
 end
