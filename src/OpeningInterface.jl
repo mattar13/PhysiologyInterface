745a90938076acd09c0bc1dd81db19048bad3a55
mattar13 <mtarchick13@gmail.com>
2023-08-29 15:55:25 -0400

    added OpeningInterface to include

--- src/OpeningInterface.jl
+++ src/OpeningInterface.jl
@@ -1,3 +1,12 @@
+#This function will walk you through a tutorial of using ElectroPhysiology
+function run_tutorials(;number = 1)
+     println("Beginning tutorial")
+     Pluto.run(notebook = "notebooks\\Tutorials1_OpeningFiles.jl")
+     println("Completed")
+end
+
+
+
 #Pluto.run(notebook = "src/Interface/test_interface.jl")
 run_experiment_analysis() = Pluto.run(notebook="src\\experiment_analysis.jl")
 run_datasheet_analysis() = Pluto.run(notebook="src\\datasheet_analysis.jl")
